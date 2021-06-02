09ba263720819ef3e4610d0d6d82522d04809eea
Fankouzu <1046166@qq.com>
2021-06-02 21:12:03 +0800

    first commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,6 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+]
